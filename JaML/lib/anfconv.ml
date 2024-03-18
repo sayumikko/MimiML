i-pankrat/comp23hw

Change the pattern for generating new names
Signed-off-by: Maxim Drumov <Drumov.M.A@gmail.com>

JaML/lib/anfconv.ml
@@ -104,13 +104,13 @@ let anf env e expr_with_hole =
     | LVar (name, _) ->
       (match Env.get_opt env name with
        | Some 0 ->
-         let* gl_name = fresh "#global_var" in
+         let* gl_name = fresh "_global_var" in
          let gl_immid = ImmId gl_name in
          let immid = ImmId name in
          let* hole = expr_with_hole gl_immid in
          return (ALet (gl_name, CApp (immid, []), hole))
        | Some n ->
-         let* cl_name = fresh "#empty_closure" in
+         let* cl_name = fresh "_empty_closure" in
          let cl_immid = ImmId cl_name in
          let immid = ImmId name in
          let* hole = expr_with_hole cl_immid in
@@ -119,30 +119,30 @@ let anf env e expr_with_hole =
     | LBinop ((op, _), e1, e2) ->
       helper e1 (fun limm ->
         helper e2 (fun rimm ->
-          let* new_name = fresh "#binop" in
+          let* new_name = fresh "_binop" in
           let* hole = expr_with_hole @@ ImmId new_name in
           return (ALet (new_name, CBinOp (op, limm, rimm), hole))))
     | LApp _ as application ->
       let construct_app expr_with_hole imm args =
-        let* new_name = fresh "#app" in
+        let* new_name = fresh "_app" in
         let* hole = expr_with_hole (ImmId new_name) in
         return (ALet (new_name, CApp (imm, args), hole))
       in
       let construct_closure expr_with_hole imm args =
-        let* new_name = fresh "#closure" in
+        let* new_name = fresh "_closure" in
         let* hole = expr_with_hole (ImmId new_name) in
         let _, n = Stdlib.Option.get @@ is_imm_top_declaration env imm in
         return (ALet (new_name, CMakeClosure (imm, n, List.length args, args), hole))
       in
       let construct_add_args_to_closure expr_with_hole imm args =
-        let* new_name = fresh "#closure" in
+        let* new_name = fresh "_closure" in
         let* hole = expr_with_hole (ImmId new_name) in
         return (ALet (new_name, CAddArgsToClosure (imm, List.length args, args), hole))
       in
       let construct_app_add_args_to_closure expr_with_hole imm app_args cl_args =
         let app = CApp (imm, app_args) in
-        let* new_app = fresh "#app" in
-        let* new_closure = fresh "#closure" in
+        let* new_app = fresh "_app" in
+        let* new_closure = fresh "_closure" in
         let* hole = expr_with_hole (ImmId new_closure) in
         return
           (ALet
@@ -193,11 +193,11 @@ let anf env e expr_with_hole =
       helper i (fun immif ->
         let* athen = helper t (fun immthen -> return @@ ACEexpr (CImmExpr immthen)) in
         let* aelse = helper e (fun immelse -> return @@ ACEexpr (CImmExpr immelse)) in
-        let* new_name = fresh "#if" in
+        let* new_name = fresh "_if" in
         let* hole = expr_with_hole @@ ImmId new_name in
         return @@ ALet (new_name, CIfThenElse (immif, athen, aelse), hole))
     | LTuple (elems, _) ->
-      let* new_name = fresh "#tuple" in
+      let* new_name = fresh "_tuple" in
       let rec tuple_helper l = function
         | hd :: tl -> helper hd (fun imm -> tuple_helper (imm :: l) tl)
         | _ ->
@@ -207,7 +207,7 @@ let anf env e expr_with_hole =
       tuple_helper [] elems
     | LTake (lexpr, n) ->
       helper lexpr (fun imm ->
-        let* new_name = fresh "#take" in
+        let* new_name = fresh "_take" in
         let* hole = expr_with_hole (ImmId new_name) in
         return (ALet (new_name, CTake (imm, n), hole)))
   in

JaML/lib/closure.ml
@@ -174,7 +174,7 @@ and closure_expr env known = function
     in
     let diff = NameS.diff fv_known known |> NameS.elements in
     let e1, ty = put_diff_arg diff (expr, ty2) in
-    let* new_id = fresh "#closure_fun" in
+    let* new_id = fresh "_closure_fun" in
     let env =
       let constr_new_let e2 = TLetIn (TPVar (new_id, ty), e1, e2) in
       extend_env new_id (diff, ty, Some constr_new_let) env

JaML/lib/lambdalift.ml
@@ -109,7 +109,7 @@ let rec get_args_let (known, expr_with_hole) = function
   | TFun (TPVar (id, ty), expr, _) ->
     get_args_let (Used (id, ty) :: known, expr_with_hole) expr
   | TFun (TPTuple (pat_lst, ty), expr, _) ->
-    let* new_id = fresh "#tuple_arg" in
+    let* new_id = fresh "_tuple_arg" in
     let take_constr = LVar (new_id, ty) in
     let expr_with_hole, _, _, _ =
       dispose_of_tuple_in pat_lst expr_with_hole take_constr take_constr
@@ -176,7 +176,7 @@ let rec lambda_lift_expr env = function
   | TLetIn (TPTuple (pat_lst, ty), e1, e2) ->
     let* e1, env = lambda_lift_expr env e1 in
     let* e2, env = lambda_lift_expr env e2 in
-    let* new_id = fresh "#tuple_out" in
+    let* new_id = fresh "_tuple_out" in
     let expr_with_hole, _ =
       dispose_of_pattern_in
         pat_lst
@@ -188,7 +188,7 @@ let rec lambda_lift_expr env = function
   | TLetIn (TPWildcard ty, e1, e2) ->
     let* e1, env = lambda_lift_expr env e1 in
     let* e2, env = lambda_lift_expr env e2 in
-    let* new_id = fresh "#wildcard" in
+    let* new_id = fresh "_wildcard" in
     return (LLetIn ((new_id, ty), e1, e2), env)
 ;;
 
@@ -200,12 +200,12 @@ let lambda_lift_bindings env = function
   | TLet (TPTuple (pat_lst, ty), expr) ->
     let* args, expr_with_pat_hole = get_args_let ([], fun x -> x) expr in
     let* expr, env = lambda_lift_expr env expr in
-    let* new_id = fresh "#tuple_out" in
+    let* new_id = fresh "_tuple_out" in
     let lst, _ = dispose_of_pattern pat_lst [] 0 (LVar (new_id, ty)) in
     return (LLet ((new_id, ty), List.rev args, expr_with_pat_hole expr), env, lst)
   | TLet (TPWildcard ty, expr) ->
     let* expr, env = lambda_lift_expr env expr in
-    let* new_id = fresh "#wildcard" in
+    let* new_id = fresh "_wildcard" in
     return (LLet ((new_id, ty), [], expr), env, [])
   | TLetRec ((id, ty), expr) ->
     let* args, expr_with_pat_hole = get_args_let ([], fun x -> x) expr in

JaML/lib/tests/closure_tests.ml
@@ -45,8 +45,8 @@ let%expect_test _ =
   [%expect
     {|
     let x = fun y ->
-        let #closure_fun1 = fun x -> x in
-        let z = fun y -> fun a -> (a (y + 1)) in ((z y) #closure_fun1)
+        let _closure_fun1 = fun x -> x in
+        let z = fun y -> fun a -> (a (y + 1)) in ((z y) _closure_fun1)
  |}]
 ;;
 
@@ -67,10 +67,10 @@ let%expect_test _ =
   [%expect
     {|
     let fac = fun n ->
-        let #closure_fun1 = fun k -> fun n -> fun m -> (k (m * n)) in
-        let #closure_fun2 = fun x -> x in
+        let _closure_fun1 = fun k -> fun n -> fun m -> (k (m * n)) in
+        let _closure_fun2 = fun x -> x in
         let rec fack = fun n -> fun k ->
-        if (n <= 1) then (k 1) else ((fack (n - 1)) ((#closure_fun1 k) n)) in ((fack n) #closure_fun2)
+        if (n <= 1) then (k 1) else ((fack (n - 1)) ((_closure_fun1 k) n)) in ((fack n) _closure_fun2)
  |}]
 ;;
 
@@ -92,11 +92,11 @@ let%expect_test _ =
   [%expect
     {|
     let fibo = fun n ->
-        let #closure_fun1 = fun x -> fun acc -> fun y -> (acc (x + y)) in
-        let #closure_fun2 = fun n -> fun fibo_cps -> fun acc -> fun x -> ((fibo_cps (n - 2)) ((#closure_fun1 x) acc)) in
-        let #closure_fun3 = fun x -> x in
+        let _closure_fun1 = fun x -> fun acc -> fun y -> (acc (x + y)) in
+        let _closure_fun2 = fun n -> fun fibo_cps -> fun acc -> fun x -> ((fibo_cps (n - 2)) ((_closure_fun1 x) acc)) in
+        let _closure_fun3 = fun x -> x in
         let rec fibo_cps = fun n -> fun acc ->
-        if (n < 3) then (acc 1) else ((fibo_cps (n - 1)) (((#closure_fun2 n) fibo_cps) acc)) in ((fibo_cps n) #closure_fun3)
+        if (n < 3) then (acc 1) else ((fibo_cps (n - 1)) (((_closure_fun2 n) fibo_cps) acc)) in ((fibo_cps n) _closure_fun3)
  |}]
 ;;
 
@@ -173,8 +173,8 @@ let%expect_test _ =
   [%expect
     {|
     let f = fun x ->
-        let #closure_fun1 = fun (k, j) -> fun a -> ((k + j) * a) in
-        let sum = fun x -> fun a -> ((#closure_fun1 x) a) in
+        let _closure_fun1 = fun (k, j) -> fun a -> ((k + j) * a) in
+        let sum = fun x -> fun a -> ((_closure_fun1 x) a) in
         let fst = fun (a, _) -> a in
         let scd = fun (_, b) -> b in (((sum x) (fst x)), ((sum x) (scd x)))
  |}]

JaML/lib/tests/lambdalift_tests.ml
@@ -110,9 +110,9 @@ let%expect_test _ =
   in
   [%expect
     {|
-    let #tuple_out1  = (1, 2);
-    let a  = take(#tuple_out1, 0);
-    let b  = take(#tuple_out1, 1)
+    let _tuple_out1  = (1, 2);
+    let a  = take(_tuple_out1, 0);
+    let b  = take(_tuple_out1, 1)
     |}]
 ;;
 
@@ -123,7 +123,7 @@ let%expect_test _ =
     |} in
     run_lambda_test e
   in
-  [%expect {| let #tuple_out1  = (1, 2) |}]
+  [%expect {| let _tuple_out1  = (1, 2) |}]
 ;;
 
 let%expect_test _ =
@@ -137,9 +137,9 @@ let%expect_test _ =
   [%expect
     {|
     let make_tuple a b = (a, b);
-    let #tuple_out1  = make_tuple 1 2;
-    let f  = take(#tuple_out1, 0);
-    let s  = take(#tuple_out1, 1)
+    let _tuple_out1  = make_tuple 1 2;
+    let f  = take(_tuple_out1, 0);
+    let s  = take(_tuple_out1, 1)
     |}]
 ;;
 
@@ -156,10 +156,10 @@ let%expect_test _ =
   [%expect
     {|
     let make_tuple a b = (a, b);
-    let #tuple_out1  = make_tuple 1 (2, 4, 3);
-    let f  = take(#tuple_out1, 0);
-    let s  = take(take(#tuple_out1, 1), 0);
-    let x  = take(take(#tuple_out1, 1), 2)
+    let _tuple_out1  = make_tuple 1 (2, 4, 3);
+    let f  = take(_tuple_out1, 0);
+    let s  = take(take(_tuple_out1, 1), 0);
+    let x  = take(take(_tuple_out1, 1), 2)
     |}]
 ;;
 
@@ -179,12 +179,12 @@ let%expect_test _ =
     {|
     let make_tuple a b = (a, b);
     let x  =
-        let #tuple_out1 = make_tuple 1 (2, (4, 5), 3) in
-        let f = take(#tuple_out1, 0) in
-        let s = take(take(#tuple_out1, 1), 0) in
-        let k = take(take(take(#tuple_out1, 1), 1), 0) in
-        let l = take(take(take(#tuple_out1, 1), 1), 1) in
-        let x = take(take(take(#tuple_out1, 1), 1), 2) in (f, s)
+        let _tuple_out1 = make_tuple 1 (2, (4, 5), 3) in
+        let f = take(_tuple_out1, 0) in
+        let s = take(take(_tuple_out1, 1), 0) in
+        let k = take(take(take(_tuple_out1, 1), 1), 0) in
+        let l = take(take(take(_tuple_out1, 1), 1), 1) in
+        let x = take(take(take(_tuple_out1, 1), 1), 2) in (f, s)
     |}]
 ;;
 
@@ -199,14 +199,14 @@ let%expect_test _ =
   in
   [%expect
     {|
-      let x #tuple_arg1 k #tuple_arg2 l =
-          let f = take(#tuple_arg1, 0) in
-          let s = take(#tuple_arg1, 1) in
-          let y = take(#tuple_arg1, 2) in
-          let s = take(#tuple_arg2, 0) in
-          let m = take(take(#tuple_arg2, 1), 0) in
-          let d = take(take(#tuple_arg2, 1), 1) in
-          let u = take(take(#tuple_arg2, 1), 2) in ((((f + s) + y) / k) * ((((s + m) + d) + u) / l)) 
+      let x _tuple_arg1 k _tuple_arg2 l =
+          let f = take(_tuple_arg1, 0) in
+          let s = take(_tuple_arg1, 1) in
+          let y = take(_tuple_arg1, 2) in
+          let s = take(_tuple_arg2, 0) in
+          let m = take(take(_tuple_arg2, 1), 0) in
+          let d = take(take(_tuple_arg2, 1), 1) in
+          let u = take(take(_tuple_arg2, 1), 2) in ((((f + s) + y) / k) * ((((s + m) + d) + u) / l))
     |}]
 ;;
 
@@ -248,11 +248,11 @@ let%expect_test _ =
   in
   [%expect
     {|
-      let sum_cortage #tuple_arg1 =
-          let a = take(take(#tuple_arg1, 0), 0) in
-          let b = take(take(#tuple_arg1, 0), 1) in
-          let d = take(take(#tuple_arg1, 1), 0) in
-          let x = take(take(#tuple_arg1, 3), 0) in
-          let y = take(take(#tuple_arg1, 3), 1) in ((((a + b) + d) + x) + y)
+      let sum_cortage _tuple_arg1 =
+          let a = take(take(_tuple_arg1, 0), 0) in
+          let b = take(take(_tuple_arg1, 0), 1) in
+          let d = take(take(_tuple_arg1, 1), 0) in
+          let x = take(take(_tuple_arg1, 3), 0) in
+          let y = take(take(_tuple_arg1, 3), 1) in ((((a + b) + d) + x) + y)
     |}]
 ;;

sayumikko/MimiML

JaML/lib/anfconv.ml
@@ -104,13 +104,13 @@ let anf env e expr_with_hole =
     | LVar (name, _) ->
       (match Env.get_opt env name with
        | Some 0 ->
-         let* gl_name = fresh "#global_var" in
+         let* gl_name = fresh "global_var" in
          let gl_immid = ImmId gl_name in
          let immid = ImmId name in
          let* hole = expr_with_hole gl_immid in
          return (ALet (gl_name, CApp (immid, []), hole))
        | Some n ->
-         let* cl_name = fresh "#empty_closure" in
+         let* cl_name = fresh "empty_closure" in
          let cl_immid = ImmId cl_name in
          let immid = ImmId name in
          let* hole = expr_with_hole cl_immid in
@@ -119,30 +119,30 @@ let anf env e expr_with_hole =
     | LBinop ((op, _), e1, e2) ->
       helper e1 (fun limm ->
         helper e2 (fun rimm ->
-          let* new_name = fresh "#binop" in
+          let* new_name = fresh "binop" in
           let* hole = expr_with_hole @@ ImmId new_name in
           return (ALet (new_name, CBinOp (op, limm, rimm), hole))))
     | LApp _ as application ->
       let construct_app expr_with_hole imm args =
-        let* new_name = fresh "#app" in
+        let* new_name = fresh "app" in
         let* hole = expr_with_hole (ImmId new_name) in
         return (ALet (new_name, CApp (imm, args), hole))
       in
       let construct_closure expr_with_hole imm args =
-        let* new_name = fresh "#closure" in
+        let* new_name = fresh "closure" in
         let* hole = expr_with_hole (ImmId new_name) in
         let _, n = Stdlib.Option.get @@ is_imm_top_declaration env imm in
         return (ALet (new_name, CMakeClosure (imm, n, List.length args, args), hole))
       in
       let construct_add_args_to_closure expr_with_hole imm args =
-        let* new_name = fresh "#closure" in
+        let* new_name = fresh "closure" in
         let* hole = expr_with_hole (ImmId new_name) in
         return (ALet (new_name, CAddArgsToClosure (imm, List.length args, args), hole))
       in
       let construct_app_add_args_to_closure expr_with_hole imm app_args cl_args =
         let app = CApp (imm, app_args) in
-        let* new_app = fresh "#app" in
-        let* new_closure = fresh "#closure" in
+        let* new_app = fresh "app" in
+        let* new_closure = fresh "closure" in
         let* hole = expr_with_hole (ImmId new_closure) in
         return
           (ALet
@@ -193,11 +193,11 @@ let anf env e expr_with_hole =
       helper i (fun immif ->
         let* athen = helper t (fun immthen -> return @@ ACEexpr (CImmExpr immthen)) in
         let* aelse = helper e (fun immelse -> return @@ ACEexpr (CImmExpr immelse)) in
-        let* new_name = fresh "#if" in
+        let* new_name = fresh "if" in
         let* hole = expr_with_hole @@ ImmId new_name in
         return @@ ALet (new_name, CIfThenElse (immif, athen, aelse), hole))
     | LTuple (elems, _) ->
-      let* new_name = fresh "#tuple" in
+      let* new_name = fresh "tuple" in
       let rec tuple_helper l = function
         | hd :: tl -> helper hd (fun imm -> tuple_helper (imm :: l) tl)
         | _ ->
@@ -207,7 +207,7 @@ let anf env e expr_with_hole =
       tuple_helper [] elems
     | LTake (lexpr, n) ->
       helper lexpr (fun imm ->
-        let* new_name = fresh "#take" in
+        let* new_name = fresh "take" in
         let* hole = expr_with_hole (ImmId new_name) in
         return (ALet (new_name, CTake (imm, n), hole)))
   in

JaML/lib/closure.ml
@@ -174,7 +174,7 @@ and closure_expr env known = function
     in
     let diff = NameS.diff fv_known known |> NameS.elements in
     let e1, ty = put_diff_arg diff (expr, ty2) in
-    let* new_id = fresh "#closure_fun" in
+    let* new_id = fresh "closure_fun" in
     let env =
       let constr_new_let e2 = TLetIn (TPVar (new_id, ty), e1, e2) in
       extend_env new_id (diff, ty, Some constr_new_let) env

JaML/lib/lambdalift.ml
@@ -109,7 +109,7 @@ let rec get_args_let (known, expr_with_hole) = function
   | TFun (TPVar (id, ty), expr, _) ->
     get_args_let (Used (id, ty) :: known, expr_with_hole) expr
   | TFun (TPTuple (pat_lst, ty), expr, _) ->
-    let* new_id = fresh "#tuple_arg" in
+    let* new_id = fresh "tuple_arg" in
     let take_constr = LVar (new_id, ty) in
     let expr_with_hole, _, _, _ =
       dispose_of_tuple_in pat_lst expr_with_hole take_constr take_constr
@@ -176,7 +176,7 @@ let rec lambda_lift_expr env = function
   | TLetIn (TPTuple (pat_lst, ty), e1, e2) ->
     let* e1, env = lambda_lift_expr env e1 in
     let* e2, env = lambda_lift_expr env e2 in
-    let* new_id = fresh "#tuple_out" in
+    let* new_id = fresh "tuple_out" in
     let expr_with_hole, _ =
       dispose_of_pattern_in
         pat_lst
@@ -188,7 +188,7 @@ let rec lambda_lift_expr env = function
   | TLetIn (TPWildcard ty, e1, e2) ->
     let* e1, env = lambda_lift_expr env e1 in
     let* e2, env = lambda_lift_expr env e2 in
-    let* new_id = fresh "#wildcard" in
+    let* new_id = fresh "wildcard" in
     return (LLetIn ((new_id, ty), e1, e2), env)
 ;;
 
@@ -200,12 +200,12 @@ let lambda_lift_bindings env = function
   | TLet (TPTuple (pat_lst, ty), expr) ->
     let* args, expr_with_pat_hole = get_args_let ([], fun x -> x) expr in
     let* expr, env = lambda_lift_expr env expr in
-    let* new_id = fresh "#tuple_out" in
+    let* new_id = fresh "tuple_out" in
     let lst, _ = dispose_of_pattern pat_lst [] 0 (LVar (new_id, ty)) in
     return (LLet ((new_id, ty), List.rev args, expr_with_pat_hole expr), env, lst)
   | TLet (TPWildcard ty, expr) ->
     let* expr, env = lambda_lift_expr env expr in
-    let* new_id = fresh "#wildcard" in
+    let* new_id = fresh "wildcard" in
     return (LLet ((new_id, ty), [], expr), env, [])
   | TLetRec ((id, ty), expr) ->
     let* args, expr_with_pat_hole = get_args_let ([], fun x -> x) expr in

JaML/lib/tests/closure_tests.ml
@@ -45,8 +45,8 @@ let%expect_test _ =
   [%expect
     {|
     let x = fun y ->
-        let #closure_fun1 = fun x -> x in
-        let z = fun y -> fun a -> (a (y + 1)) in ((z y) #closure_fun1)
+        let closure_fun1 = fun x -> x in
+        let z = fun y -> fun a -> (a (y + 1)) in ((z y) closure_fun1)
  |}]
 ;;
 
@@ -67,10 +67,10 @@ let%expect_test _ =
   [%expect
     {|
     let fac = fun n ->
-        let #closure_fun1 = fun k -> fun n -> fun m -> (k (m * n)) in
-        let #closure_fun2 = fun x -> x in
+        let closure_fun1 = fun k -> fun n -> fun m -> (k (m * n)) in
+        let closure_fun2 = fun x -> x in
         let rec fack = fun n -> fun k ->
-        if (n <= 1) then (k 1) else ((fack (n - 1)) ((#closure_fun1 k) n)) in ((fack n) #closure_fun2)
+        if (n <= 1) then (k 1) else ((fack (n - 1)) ((closure_fun1 k) n)) in ((fack n) closure_fun2)
  |}]
 ;;
 
@@ -92,11 +92,11 @@ let%expect_test _ =
   [%expect
     {|
     let fibo = fun n ->
-        let #closure_fun1 = fun x -> fun acc -> fun y -> (acc (x + y)) in
-        let #closure_fun2 = fun n -> fun fibo_cps -> fun acc -> fun x -> ((fibo_cps (n - 2)) ((#closure_fun1 x) acc)) in
-        let #closure_fun3 = fun x -> x in
+        let closure_fun1 = fun x -> fun acc -> fun y -> (acc (x + y)) in
+        let closure_fun2 = fun n -> fun fibo_cps -> fun acc -> fun x -> ((fibo_cps (n - 2)) ((closure_fun1 x) acc)) in
+        let closure_fun3 = fun x -> x in
         let rec fibo_cps = fun n -> fun acc ->
-        if (n < 3) then (acc 1) else ((fibo_cps (n - 1)) (((#closure_fun2 n) fibo_cps) acc)) in ((fibo_cps n) #closure_fun3)
+        if (n < 3) then (acc 1) else ((fibo_cps (n - 1)) (((closure_fun2 n) fibo_cps) acc)) in ((fibo_cps n) closure_fun3)
  |}]
 ;;
 
@@ -173,8 +173,8 @@ let%expect_test _ =
   [%expect
     {|
     let f = fun x ->
-        let #closure_fun1 = fun (k, j) -> fun a -> ((k + j) * a) in
-        let sum = fun x -> fun a -> ((#closure_fun1 x) a) in
+        let closure_fun1 = fun (k, j) -> fun a -> ((k + j) * a) in
+        let sum = fun x -> fun a -> ((closure_fun1 x) a) in
         let fst = fun (a, _) -> a in
         let scd = fun (_, b) -> b in (((sum x) (fst x)), ((sum x) (scd x)))
  |}]

JaML/lib/tests/lambdalift_tests.ml
@@ -110,9 +110,9 @@ let%expect_test _ =
   in
   [%expect
     {|
-    let #tuple_out1  = (1, 2);
-    let a  = take(#tuple_out1, 0);
-    let b  = take(#tuple_out1, 1)
+    let tuple_out1  = (1, 2);
+    let a  = take(tuple_out1, 0);
+    let b  = take(tuple_out1, 1)
     |}]
 ;;
 
@@ -123,7 +123,7 @@ let%expect_test _ =
     |} in
     run_lambda_test e
   in
-  [%expect {| let #tuple_out1  = (1, 2) |}]
+  [%expect {| let tuple_out1  = (1, 2) |}]
 ;;
 
 let%expect_test _ =
@@ -137,9 +137,9 @@ let%expect_test _ =
   [%expect
     {|
     let make_tuple a b = (a, b);
-    let #tuple_out1  = make_tuple 1 2;
-    let f  = take(#tuple_out1, 0);
-    let s  = take(#tuple_out1, 1)
+    let tuple_out1  = make_tuple 1 2;
+    let f  = take(tuple_out1, 0);
+    let s  = take(tuple_out1, 1)
     |}]
 ;;
 
@@ -156,10 +156,10 @@ let%expect_test _ =
   [%expect
     {|
     let make_tuple a b = (a, b);
-    let #tuple_out1  = make_tuple 1 (2, 4, 3);
-    let f  = take(#tuple_out1, 0);
-    let s  = take(take(#tuple_out1, 1), 0);
-    let x  = take(take(#tuple_out1, 1), 2)
+    let tuple_out1  = make_tuple 1 (2, 4, 3);
+    let f  = take(tuple_out1, 0);
+    let s  = take(take(tuple_out1, 1), 0);
+    let x  = take(take(tuple_out1, 1), 2)
     |}]
 ;;
 
@@ -179,12 +179,12 @@ let%expect_test _ =
     {|
     let make_tuple a b = (a, b);
     let x  =
-        let #tuple_out1 = make_tuple 1 (2, (4, 5), 3) in
-        let f = take(#tuple_out1, 0) in
-        let s = take(take(#tuple_out1, 1), 0) in
-        let k = take(take(take(#tuple_out1, 1), 1), 0) in
-        let l = take(take(take(#tuple_out1, 1), 1), 1) in
-        let x = take(take(take(#tuple_out1, 1), 1), 2) in (f, s)
+        let tuple_out1 = make_tuple 1 (2, (4, 5), 3) in
+        let f = take(tuple_out1, 0) in
+        let s = take(take(tuple_out1, 1), 0) in
+        let k = take(take(take(tuple_out1, 1), 1), 0) in
+        let l = take(take(take(tuple_out1, 1), 1), 1) in
+        let x = take(take(take(tuple_out1, 1), 1), 2) in (f, s)
     |}]
 ;;
 
@@ -199,14 +199,14 @@ let%expect_test _ =
   in
   [%expect
     {|
-      let x #tuple_arg1 k #tuple_arg2 l =
-          let f = take(#tuple_arg1, 0) in
-          let s = take(#tuple_arg1, 1) in
-          let y = take(#tuple_arg1, 2) in
-          let s = take(#tuple_arg2, 0) in
-          let m = take(take(#tuple_arg2, 1), 0) in
-          let d = take(take(#tuple_arg2, 1), 1) in
-          let u = take(take(#tuple_arg2, 1), 2) in ((((f + s) + y) / k) * ((((s + m) + d) + u) / l)) 
+      let x tuple_arg1 k tuple_arg2 l =
+          let f = take(tuple_arg1, 0) in
+          let s = take(tuple_arg1, 1) in
+          let y = take(tuple_arg1, 2) in
+          let s = take(tuple_arg2, 0) in
+          let m = take(take(tuple_arg2, 1), 0) in
+          let d = take(take(tuple_arg2, 1), 1) in
+          let u = take(take(tuple_arg2, 1), 2) in ((((f + s) + y) / k) * ((((s + m) + d) + u) / l))
     |}]
 ;;
 
@@ -248,11 +248,11 @@ let%expect_test _ =
   in
   [%expect
     {|
-      let sum_cortage #tuple_arg1 =
-          let a = take(take(#tuple_arg1, 0), 0) in
-          let b = take(take(#tuple_arg1, 0), 1) in
-          let d = take(take(#tuple_arg1, 1), 0) in
-          let x = take(take(#tuple_arg1, 3), 0) in
-          let y = take(take(#tuple_arg1, 3), 1) in ((((a + b) + d) + x) + y)
+      let sum_cortage tuple_arg1 =
+          let a = take(take(tuple_arg1, 0), 0) in
+          let b = take(take(tuple_arg1, 0), 1) in
+          let d = take(take(tuple_arg1, 1), 0) in
+          let x = take(take(tuple_arg1, 3), 0) in
+          let y = take(take(tuple_arg1, 3), 1) in ((((a + b) + d) + x) + y)
     |}]
 ;;